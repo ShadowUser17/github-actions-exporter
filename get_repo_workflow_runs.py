b71723ea7d0b7b9a53edcfaea50420bdaf6b335a
ShadowUser17 <linuxforce17@gmail.com>
2024-05-21 13:17:25 +0300

    Add get_repo_workflow_runs.py

---
+++ get_repo_workflow_runs.py
@@ -0,0 +1,54 @@
+import os
+import sys
+import argparse
+import traceback
+
+from github import Auth
+from github import Github
+from github.Repository import Repository
+from github.WorkflowRun import WorkflowRun
+from github.Organization import Organization
+
+# DEBUG_MODE
+# GITHUB_ORG
+# GITHUB_TOKEN
+
+
+def get_github_client(token: str = "") -> Github:
+    return Github(auth=Auth.Token(os.environ.get("GITHUB_TOKEN", token)))
+
+
+def get_github_org(client: Github, org: str = "") -> Organization:
+    return client.get_organization(os.environ.get("GITHUB_ORG", org))
+
+
+def get_github_repo_list(org: Organization, repos_type: str = "") -> list[Repository]:
+    return list(org.get_repos(type=repos_type))
+
+
+def get_repo_workflow_runs_list(repo: Repository, status: str = "") -> list[WorkflowRun]:
+    return list(repo.get_workflow_runs(status=status))
+
+
+def parse_args() -> argparse.Namespace:
+    parser = argparse.ArgumentParser()
+    repo_type = ('all', 'public', 'private', 'forks', 'sources', 'member')
+    run_status = ('queued', 'in_progress', 'completed', 'success', 'failure', 'neutral', 'cancelled', 'skipped', 'timed_out', 'action_required')
+    parser.add_argument("-t", "--token", dest="token", default="", help="Set GitHub personal token.")
+    parser.add_argument("-o", "--org", dest="org", default="", help="Set GitHub organization.")
+    parser.add_argument("-r", "--type", dest="type", default="sources", choices=repo_type, help="Select type of repositories.")
+    parser.add_argument("-s", "--status", dest="status", default="", choices=run_status, help="Select status of workflow runners.")
+    return parser.parse_args()
+
+
+try:
+    args = parse_args()
+    org = get_github_org(client=get_github_client(token=args.token), org=args.org)
+
+    for repo in get_github_repo_list(org=org, repos_type=args.type):
+        for run in get_repo_workflow_runs_list(repo=repo, status=args.status):
+            print("{}: ({}: {}/{})".format(repo.name, run.name, run.status, run.conclusion))
+
+except Exception:
+    traceback.print_exc()
+    sys.exit(1)
